a2bc9336496d341dcb6fe1d8f0de16ad30246f8f
crazyrabbit <1210454611@qq.com>
2018-11-30 16:36:00 +0800

    test 112

--- service-worker.js
+++ service-worker.js
@@ -17,7 +17,7 @@ const workboxSW = new WorkboxSW({
 workboxSW.precache([
   {
     "url": "index.html",
-    "revision": "fec8ce4d4933d01c15f746acc8da3617"
+    "revision": "36b57a4cf10a509698426823881b6abf"
   },
   {
     "url": "static/api/guc_api.js",
@@ -32,7 +32,7 @@ workboxSW.precache([
     "revision": "c95c54a802b6d4b7099113d7fa5924f3"
   },
   {
-    "url": "static/css/index.736796fa.css"
+    "url": "static/css/index.b0187666.css"
   },
   {
     "url": "static/img/sprites/sprite.css",
@@ -42,7 +42,7 @@ workboxSW.precache([
     "url": "static/js/0.d830d557.js"
   },
   {
-    "url": "static/js/index.96a4a5f2.js"
+    "url": "static/js/index.414c2d72.js"
   },
   {
     "url": "static/js/manifest.13eb4f4a.js"
